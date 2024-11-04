2e4ddd909649c21792e45f1edfc421e6d44535f5
Elides13 <cerezo.1617@gmail.com>
2024-11-04 13:36:39 -0400

    segundo commit

---
+++ Parcial2/ClnParcial2Ecg/SerieCln.cs
@@ -0,0 +1,80 @@
+﻿using CadParcial2Ecg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnParcial2Ecg
+{
+    public class ClnSerie
+    {
+        public static int insertar(Serie serie)
+        {
+            using (var context = new Parcial2EcgEntities())
+            {
+                context.Serie.Add(serie);
+                context.SaveChanges();
+                return serie.id;
+            }
+        }
+
+        public static int actualizar(Serie serie)
+        {
+            using (var context = new Parcial2EcgEntities())
+            {
+                var existente = context.Serie.Find(serie.id);
+                if (existente != null)
+                {
+                    existente.titulo = serie.titulo;
+                    existente.sinopsis = serie.sinopsis;
+                    existente.director = serie.director;
+                    existente.episodios = serie.episodios;
+                    existente.fechaEstreno = serie.fechaEstreno;
+                    existente.estado = serie.estado;
+                    return context.SaveChanges();
+                }
+                return 0;
+            }
+        }
+
+        public static int eliminar(int id, string usuario)
+        {
+            using (var context = new Parcial2EcgEntities())
+            {
+                var serie = context.Serie.Find(id);
+                if (serie != null)
+                {
+                    serie.estado = -1;
+                    return context.SaveChanges();
+                }
+                return 0;
+            }
+        }
+
+        public static Serie obtenerUno(int id)
+        {
+            using (var context = new Parcial2EcgEntities())
+            {
+                return context.Serie.Find(id);
+            }
+        }
+
+        public static List<Serie> listar()
+        {
+            using (var context = new Parcial2EcgEntities())
+            {
+                return context.Serie.Where(x => x.estado != -1).ToList();
+            }
+        }
+
+        public static List<paSerieListar_Result> listarPa(string parametro)
+        {
+            using (var context = new Parcial2EcgEntities())
+            {
+                return context.paSerieListar(parametro).ToList();
+            }
+        }
+
+    }
+}
---
+++ Parcial2/ddl_dml_parcial2.sql
@@ -0,0 +1,56 @@
+﻿CREATE DATABASE Parcial2Ecg;
+GO
+
+
+USE Parcial2Ecg;
+GO
+
+
+USE [master];
+GO
+CREATE LOGIN [usrparcial2] WITH PASSWORD=N'12345678',
+    DEFAULT_DATABASE = Parcial2Ecg,
+    CHECK_EXPIRATION = OFF,
+    CHECK_POLICY = ON;
+GO
+USE Parcial2Ecg;
+GO
+CREATE USER [usrparcial2] FOR LOGIN [usrparcial2];
+GO
+ALTER ROLE [db_owner] ADD MEMBER [usrparcial2];
+GO
+
+
+CREATE TABLE Serie (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    titulo VARCHAR(250),
+    sinopsis VARCHAR(5000),
+    director VARCHAR(100),
+    episodios INT,
+    fechaEstreno DATE,
+);
+GO
+
+ALTER TABLE Serie ADD estado SMALLINT NOT NULL DEFAULT 1; -- -1: Eliminado, 0: Inactivo, 1: Activo
+GO
+
+CREATE PROCEDURE paSerieListar 
+    @parametro VARCHAR(50)
+AS
+BEGIN
+    SELECT id, titulo, sinopsis, director, episodios, fechaEstreno
+    FROM Serie
+    WHERE estado <> -1 AND titulo LIKE '%' + REPLACE(@parametro, ' ', '%') + '%';
+END
+GO
+
+INSERT INTO Serie (titulo, sinopsis, director, episodios, fechaEstreno)
+VALUES
+('Barbie: Vida en la Casa de los Sueños', 'Barbie y sus amigos viven divertidas aventuras en una casa de ensueño', 'Conrad Helten', 75, '2012-05-11'),
+('Barbie Aventuras en la Casa de los Sueños', 'Barbie y su familia se embarcan en nuevas aventuras desde su Casa de los Sueños', 'Conrad Helten', 52, '2018-05-03'),
+('Barbie y los Rockeros', 'Barbie y su banda de rock viajan en el tiempo para hacer un concierto', 'Bernard Deyriès', 2, '1987-09-27'),
+('Barbie: Aventura de Princesa', 'Barbie cambia su vida al intercambiarse con una princesa', 'Conrad Helten', 1, '2020-09-01');
+
+
+
+SELECT * FROM SERIE
